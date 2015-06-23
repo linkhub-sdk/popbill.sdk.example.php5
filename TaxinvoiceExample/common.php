46de45d391bdc31fb4b3e22471c75929c3e21fa6
김성준 <pallet027@gmail.com>
2015-06-23 17:24:39 +0900

    Add STREAM Mode.

--- TaxinvoiceExample/common.php
+++ TaxinvoiceExample/common.php
@@ -7,6 +7,10 @@ $LinkID = 'TESTER';
 //발급받은 비밀키. 유출에 주의하시기 바랍니다.
 $SecretKey = 'SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=';
 
+//통신방식 기본은 CURL , curl 사용에 문제가 있을경우 STREAM 사용가능.
+//STREAM 사용시에는 allow_fopen_url = on 으로 설정해야함.
+define('LINKHUB_COMM_MODE','CURL');
+
 $TaxinvoiceService = new TaxinvoiceService($LinkID,$SecretKey);
 
 //테스트모드로 설정되면 test.popbill.com으로 연결됩니다.
