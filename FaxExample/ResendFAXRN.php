88b9702b8d0b150b6f02c2655bbb02125044768c
kimhyunjin <khjzzm@gmail.com>
2018-11-19 09:56:13 +0900

    resendfax 전송 가능일자 변경

--- FaxExample/ResendFAXRN.php
+++ FaxExample/ResendFAXRN.php
@@ -1,90 +1,89 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="/Example.css" media="screen" />
-		<title>팝빌 SDK PHP 5.X Example.</title>
-	</head>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="/Example.css" media="screen"/>
+    <title>팝빌 SDK PHP 5.X Example.</title>
+</head>
 <?php
-  /**
-  * 전송요청번호(requestNum)을 할당한 팩스를 재전송합니다.
-  * - 전송일로부터 180일이 경과된 경우 재전송할 수 없습니다.
-  * - 팩스 재전송 요청시 포인트가 차감됩니다. (전송실패시 환불처리)
-  * - 팩스전송 문서 파일포맷 안내 : http://blog.linkhub.co.kr/2561
-  */
+/**
+ * 전송요청번호(requestNum)을 할당한 팩스를 재전송합니다.
+ * - 전송일로부터 60일이 경과된 경우 재전송할 수 없습니다.
+ * - 팩스 재전송 요청시 포인트가 차감됩니다. (전송실패시 환불처리)
+ * - 팩스전송 문서 파일포맷 안내 : http://blog.linkhub.co.kr/2561
+ */
 
-	include 'common.php';
+include 'common.php';
 
-  // 팝빌 회원 사업자번호
-	$testCorpNum = '1234567890';
+// 팝빌 회원 사업자번호
+$testCorpNum = '1234567890';
 
-  // 팝빌 회원 아이디
-  $testUserID = 'testkorea';
+// 팝빌 회원 아이디
+$testUserID = 'testkorea';
 
-  // 팩스전송 발신번호, 공백처리시 기존전송정보로 재전송
-  $Sender = '07043042991';
+// 팩스전송 발신번호, 공백처리시 기존전송정보로 재전송
+$Sender = '07043042991';
 
-  // 팩스전송 발신자명, 공백처리시 기존전송정보로 재전송
-  $SenderName = '발신자명';
+// 팩스전송 발신자명, 공백처리시 기존전송정보로 재전송
+$SenderName = '발신자명';
 
-  // 팩스 수신정보 배열, NULL로 처리하는 경우 기존전송정보로 재전송
-  $Receivers = NULL;
+// 팩스 수신정보 배열, NULL로 처리하는 경우 기존전송정보로 재전송
+$Receivers = NULL;
 
-  /*
-  // 팩스 수신정보가 기존전송정보와 다를경우 아래의 코드 참조
-	$Receivers[] = array(
-    // 팩스 수신번호
-		'rcv' => '070111222',
+/*
+// 팩스 수신정보가 기존전송정보와 다를경우 아래의 코드 참조
+  $Receivers[] = array(
+  // 팩스 수신번호
+      'rcv' => '070111222',
 
-    // 수신자명
-		'rcvnm' => '팝빌담당자'
-	);
-  */
+  // 수신자명
+      'rcvnm' => '팝빌담당자'
+  );
+*/
 
-  // 예약전송일시(yyyyMMddHHmmss) ex) 20151212230000, null인경우 즉시전송
-	$reserveDT = null;
+// 예약전송일시(yyyyMMddHHmmss) ex) 20151212230000, null인경우 즉시전송
+$reserveDT = null;
 
-  // 팩스 제목
-  $title = '팩스 재전송 제목';
+// 팩스 제목
+$title = '팩스 재전송 제목';
 
-  // 원본 팩스 전송시 할당한 전송요청번호(requestNum)
-  $originalFAXrequestNum = '';
+// 원본 팩스 전송시 할당한 전송요청번호(requestNum)
+$originalFAXrequestNum = '';
 
-	// 재전송 팩스의 전송요청번호
-	// 파트너가 전송 건에 대해 관리번호를 구성하여 관리하는 경우 사용.
-	// 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
-  // 재전송 팩스의 전송상태확인(GetSendDetailRN) / 예약전송취소(CancelReserveRN) 에 이용됩니다.
-	$requestNum = '';
+// 재전송 팩스의 전송요청번호
+// 파트너가 전송 건에 대해 관리번호를 구성하여 관리하는 경우 사용.
+// 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
+// 재전송 팩스의 전송상태확인(GetSendDetailRN) / 예약전송취소(CancelReserveRN) 에 이용됩니다.
+$requestNum = '';
 
-	try {
-		$receiptNum = $FaxService->ResendFAXRN($testCorpNum, $requestNum, $Sender,
-      $SenderName, $Receivers, $originalFAXrequestNum, $reserveDT, $testUserID, $title);
-	}
-  catch (PopbillException $pe) {
-		$code = $pe->getCode();
-		$message = $pe->getMessage();
-	}
+try {
+    $receiptNum = $FaxService->ResendFAXRN($testCorpNum, $requestNum, $Sender,
+        $SenderName, $Receivers, $originalFAXrequestNum, $reserveDT, $testUserID, $title);
+} catch (PopbillException $pe) {
+    $code = $pe->getCode();
+    $message = $pe->getMessage();
+}
 ?>
-	<body>
-		<div id="content">
-			<p class="heading1">Response</p>
-			<br/>
-			<fieldset class="fieldset1">
-				<legend>팩스 재전송 요청</legend>
-				<ul>
-					<?php
-						if ( isset($receiptNum) ) {
-					?>
-							<li>receiptNum(팩스접수번호) : <?php echo $receiptNum?></li>
-					<?php
-						} else {
-					?>
-							<li>Response.code : <?php echo $code ?> </li>
-							<li>Response.message : <?php echo $message ?></li>
-					<?php
-						}
-					?>
-				</ul>
-			</fieldset>
-		 </div>
-	</body>
+<body>
+<div id="content">
+    <p class="heading1">Response</p>
+    <br/>
+    <fieldset class="fieldset1">
+        <legend>팩스 재전송 요청</legend>
+        <ul>
+            <?php
+            if (isset($receiptNum)) {
+                ?>
+                <li>receiptNum(팩스접수번호) : <?php echo $receiptNum ?></li>
+                <?php
+            } else {
+                ?>
+                <li>Response.code : <?php echo $code ?> </li>
+                <li>Response.message : <?php echo $message ?></li>
+                <?php
+            }
+            ?>
+        </ul>
+    </fieldset>
+</div>
+</body>
 </html>
--- FaxExample/ResendFAX_Multi.php
+++ FaxExample/ResendFAX_Multi.php
@@ -1,96 +1,95 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<link rel="stylesheet" type="text/css" href="/Example.css" media="screen" />
-		<title>팝빌 SDK PHP 5.X Example.</title>
-	</head>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="/Example.css" media="screen"/>
+    <title>팝빌 SDK PHP 5.X Example.</title>
+</head>
 <?php
-  /**
-  * 팩스를 재전송합니다.
-  * - 전송일로부터 180일이 경과된 경우 재전송할 수 없습니다.
-	* - 팩스 재전송 요청시 포인트가 차감됩니다. (전송실패시 환불처리)
-  * - 팩스전송 문서 파일포맷 안내 : http://blog.linkhub.co.kr/2561
-  */
+/**
+ * 팩스를 재전송합니다.
+ * - 전송일로부터 60일이 경과된 경우 재전송할 수 없습니다.
+ * - 팩스 재전송 요청시 포인트가 차감됩니다. (전송실패시 환불처리)
+ * - 팩스전송 문서 파일포맷 안내 : http://blog.linkhub.co.kr/2561
+ */
 
-	include 'common.php';
+include 'common.php';
 
-  // 팝빌 회원 사업자번호
-	$testCorpNum = '1234567890';
+// 팝빌 회원 사업자번호
+$testCorpNum = '1234567890';
 
-  // 팝빌 회원 아이디
-  $testUserID = 'testkorea';
+// 팝빌 회원 아이디
+$testUserID = 'testkorea';
 
-  // 팩스 접수번호
-  $ReceiptNum = '018062617421600001';
+// 팩스 접수번호
+$ReceiptNum = '018062617421600001';
 
-  // 팩스전송 발신번호, 공백처리시 기존전송정보로 재전송
-  $Sender = '07043042991';
+// 팩스전송 발신번호, 공백처리시 기존전송정보로 재전송
+$Sender = '07043042991';
 
-  // 팩스전송 발신자명, 공백처리시 기존전송정보로 재전송
-  $SenderName = '발신자명';
+// 팩스전송 발신자명, 공백처리시 기존전송정보로 재전송
+$SenderName = '발신자명';
 
-  // 팩스 수신정보, NULL로 처리하는 경우 기존전송정보로 재전송
-  //$Receivers = NULL;
+// 팩스 수신정보, NULL로 처리하는 경우 기존전송정보로 재전송
+//$Receivers = NULL;
 
 
-  // 팩스 수신정보가 기존전송정보와 다르게 동보전송하는 경우 아래의 코드 참조
-	$Receivers[] = array(
+// 팩스 수신정보가 기존전송정보와 다르게 동보전송하는 경우 아래의 코드 참조
+$Receivers[] = array(
     // 팩스 수신번호
-		'rcv' => '070111222',
+    'rcv' => '070111222',
     // 팩스 수신자명
-		'rcvnm' => '팝빌담당자'
-	);
+    'rcvnm' => '팝빌담당자'
+);
 
-	$Receivers[] = array(
+$Receivers[] = array(
     // 팩스 수신번호
-		'rcv' => '070333444',
+    'rcv' => '070333444',
     // 팩스 수신자명
-		'rcvnm' => '수신담당자'
-	);
+    'rcvnm' => '수신담당자'
+);
 
 
-  // 예약전송일시(yyyyMMddHHmmss) ex)20151212230000, null인경우 즉시전송
-	$reserveDT = null;
+// 예약전송일시(yyyyMMddHHmmss) ex)20151212230000, null인경우 즉시전송
+$reserveDT = null;
 
-  // 팩스 제목
-  $title = '팩스 재전송 제목';
+// 팩스 제목
+$title = '팩스 재전송 제목';
 
-	// 재전송 팩스의 전송요청번호
-	// 파트너가 전송 건에 대해 관리번호를 구성하여 관리하는 경우 사용.
-	// 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
-  // 재전송 팩스의 전송상태확인(GetSendDetailRN) / 예약전송취소(CancelReserveRN) 에 이용됩니다.
-	$requestNum = '';
+// 재전송 팩스의 전송요청번호
+// 파트너가 전송 건에 대해 관리번호를 구성하여 관리하는 경우 사용.
+// 1~36자리로 구성. 영문, 숫자, 하이픈(-), 언더바(_)를 조합하여 팝빌 회원별로 중복되지 않도록 할당.
+// 재전송 팩스의 전송상태확인(GetSendDetailRN) / 예약전송취소(CancelReserveRN) 에 이용됩니다.
+$requestNum = '';
 
-	try {
-		$receiptNum = $FaxService->ResendFAX($testCorpNum, $ReceiptNum, $Sender,
-      $SenderName, $Receivers, $reserveDT, $testUserID, $title, $requestNum);
-	}
-  catch (PopbillException $pe) {
-		$code = $pe->getCode();
-		$message = $pe->getMessage();
-	}
+try {
+    $receiptNum = $FaxService->ResendFAX($testCorpNum, $ReceiptNum, $Sender,
+        $SenderName, $Receivers, $reserveDT, $testUserID, $title, $requestNum);
+} catch (PopbillException $pe) {
+    $code = $pe->getCode();
+    $message = $pe->getMessage();
+}
 ?>
-	<body>
-		<div id="content">
-			<p class="heading1">Response</p>
-			<br/>
-			<fieldset class="fieldset1">
-				<legend>팩스 재전송</legend>
-				<ul>
-					<?php
-						if ( isset($receiptNum) ) {
-					?>
-							<li>receiptNum (팩스접수번호) : <?php echo $receiptNum ?></li>
-					<?php
-						} else {
-					?>
-							<li>Response.code : <?php echo $code ?> </li>
-							<li>Response.message : <?php echo $message ?></li>
-					<?php
-						}
-					?>
-				</ul>
-			</fieldset>
-		 </div>
-	</body>
+<body>
+<div id="content">
+    <p class="heading1">Response</p>
+    <br/>
+    <fieldset class="fieldset1">
+        <legend>팩스 재전송</legend>
+        <ul>
+            <?php
+            if (isset($receiptNum)) {
+                ?>
+                <li>receiptNum (팩스접수번호) : <?php echo $receiptNum ?></li>
+                <?php
+            } else {
+                ?>
+                <li>Response.code : <?php echo $code ?> </li>
+                <li>Response.message : <?php echo $message ?></li>
+                <?php
+            }
+            ?>
+        </ul>
+    </fieldset>
+</div>
+</body>
 </html>
