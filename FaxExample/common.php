be70515bd2ed60c2e0a85010f47cdd8e9005bae8
kimhyunjin <khjzzm@gmail.com>
2019-01-02 16:06:00 +0900

    typo comments allow_url_fopen

--- FaxExample/common.php
+++ FaxExample/common.php
@@ -22,7 +22,7 @@
   $SecretKey = 'SwWxqU+0TErBXy/9TVjIPEnI0VTUMMSQZtJf3Ed8q3I=';
 
   // 통신방식 기본은 CURL , curl 사용에 문제가 있을경우 STREAM 사용가능.
-  // STREAM 사용시에는 php.ini의 allow_fopen_url = on 으로 설정해야함.
+  // STREAM 사용시에는 php.ini의 allow_url_fopen = on 으로 설정해야함.
   define('LINKHUB_COMM_MODE','CURL');
 
   $FaxService = new FaxService($LinkID, $SecretKey);
