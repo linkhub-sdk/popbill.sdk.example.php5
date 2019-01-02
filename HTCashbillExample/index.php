492edd1025570f13a3c41ad1c590576e593e9582
kimhyunjin <khjzzm@gmail.com>
2019-01-02 14:09:34 +0900

    HTCashbill code clean

--- HTCashbillExample/index.php
+++ HTCashbillExample/index.php
@@ -7,78 +7,64 @@
 </head>
 <body>
 <div id="content">
-
-    <p class="heading1">팝빌 홈택스 현금영수증 연계 API SDK PHP 5.x Example.</p>
-
+    <p class="heading1">팝빌 홈택스연동(현금영수증) API SDK PHP 5.x Example.</p>
     <br/>
-
     <fieldset class="fieldset1">
-        <legend>팝빌 기본 API</legend>
-
-        <fieldset class="fieldset2">
-            <legend>회원사 정보</legend>
-            <ul>
-                <li><a href="CheckIsMember.php">CheckIsMember</a> - 연동회원 가입여부 확인</li>
-                <li><a href="CheckID.php">CheckID</a> - 연동회원 아이디 중복 확인</li>
-                <li><a href="JoinMember.php">JoinMember</a> - 연동회원 가입 요청</li>
-                <li><a href="GetChargeInfo.php">GetChargeInfo</a> - 과금정보 확인</li>
-                <li><a href="GetBalance.php">GetBalance</a> - 연동회원 잔여포인트 확인</li>
-                <li><a href="GetAccessURL.php">GetAccessURL</a> - 팝빌 로그인 URL</li>
-                <li><a href="GetChargeURL.php">GetChargeURL</a> - 연동회원 포인트충전 URL</li>
-                <li><a href="GetPartnerBalance.php">GetPartnerBalance</a> - 파트너 잔여포인트 확인</li>
-                <li><a href="GetPartnerURL.php">GetPartnerURL</a> - 파트너 포인트충전 팝업 URL</li>
-                <li><a href="RegistContact.php">RegistContact</a> - 담당자 추가</li>
-                <li><a href="ListContact.php">ListContact</a> - 담당자 목록 확인</li>
-                <li><a href="UpdateContact.php">UpdateContact</a> - 담당자 정보 수정</li>
-                <li><a href="GetCorpInfo.php">GetCorpInfo</a> - 회사정보 확인</li>
-                <li><a href="UpdateCorpInfo.php">UpdateCorpInfo</a> - 회사정보 수정</li>
-            </ul>
-        </fieldset>
+        <legend>홈택스 현금영수증 매입/매출 내역 수집</legend>
+        <ul>
+            <li><a href="RequestJob.php">RequestJob</a> (수집 요청)</li>
+            <li><a href="GetJobState.php">GetJobState</a> (수집 상태 확인)</li>
+            <li><a href="ListActiveJob.php">ListActiveJob</a> (수집 상태 목록 확인)</li>
+        </ul>
     </fieldset>
-
     <br/>
-
     <fieldset class="fieldset1">
-        <legend>홈택스 현금영수증 연계 관련 API</legend>
-
-        <fieldset class="fieldset2">
-            <legend>매출/매입 내역 수집</legend>
-            <ul>
-                <li><a href="RequestJob.php">RequestJob</a> - 수집 요청</li>
-                <li><a href="GetJobState.php">GetJobState</a> - 수집 상태 확인</li>
-                <li><a href="ListActiveJob.php">ListActiveJob</a> - 수집 상태 목록 확인</li>
-            </ul>
-        </fieldset>
-
-        <fieldset class="fieldset2">
-            <legend>매출/매입 수집 결과 조회</legend>
-            <ul>
-                <li><a href="Search.php">Search</a> - 수집 결과 조회</li>
-                <li><a href="Summary.php">Summary</a> - 수집 결과 요약정보 조회</li>
-            </ul>
-        </fieldset>
-
-        <fieldset class="fieldset2">
-            <legend>부가기능</legend>
-            <ul>
-                <li><a href="GetFlatRatePopUpURL.php">GetFlatRatePopUpURL</a> - 정액제 서비스 신청 URL</li>
-                <li><a href="GetFlatRateState.php">GetFlatRateState</a> - 정액제 서비스 상태 확인</li>
-            </ul>
-        </fieldset>
-
-        <fieldset class="fieldset2">
-            <legend>홈택스 인증관련 기능</legend>
-            <ul>
-                <li><a href="GetCertificatePopUpURL.php">GetCertificatePopUpURL</a> - 홈택스연동 인증관리 팝업 URL</li>
-                <li><a href="GetCertificateExpireDate.php">GetCertificateExpireDate</a> - 홈택스연동 공인인증서 만료일자 확인</li>
-                <li><a href="CheckCertValidation.php">CheckCertValidation</a> - 홈택스 공인인증서 로그인 테스트</li>
-                <li><a href="RegistDeptUser.php">RegistDeptUser</a> - 홈택스 현금영수증 부서사용자 계정등록</li>
-                <li><a href="CheckDeptUser.php">CheckDeptUser</a> - 홈택스 부서사용자 등록정보 확인</li>
-                <li><a href="CheckLoginDeptUser.php">CheckLoginDeptUser</a> - 홈택스 부서사용자 로그인 테스트</li>
-                <li><a href="DeleteDeptUser.php">DeleteDeptUser</a> - 홈택스 부서사용자 등록정보 삭제</li>
-            </ul>
-        </fieldset>
+        <legend>홈택스 현금영수증 매입/매출 내역 수집 결과 조회</legend>
+        <ul>
+            <li><a href="Search.php">Search</a> (수집 결과 조회)</li>
+            <li><a href="Summary.php">Summary</a> (수집 결과 요약정보 조회)</li>
+        </ul>
     </fieldset>
-</div>
+    <br/>
+    <fieldset class="fieldset1">
+        <legend>홈택스연동 인증 관리</legend>
+        <ul>
+            <li><a href="GetCertificatePopUpURL.php">GetCertificatePopUpURL</a> (홈택스연동 인증 관리 팝업 URL)</li>
+            <li><a href="GetCertificateExpireDate.php">GetCertificateExpireDate</a> (홈택스연동 공인인증서 만료일자 확인)</li>
+            <li><a href="CheckCertValidation.php">CheckCertValidation</a> (홈택스 공인인증서 로그인 테스트)</li>
+            <li><a href="RegistDeptUser.php">RegistDeptUser</a> (부서사용자 계정등록)</li>
+            <li><a href="CheckDeptUser.php">CheckDeptUser</a> (부서사용자 등록정보 확인)</li>
+            <li><a href="CheckLoginDeptUser.php">CheckLoginDeptUser</a> (부서사용자 로그인 테스트)</li>
+            <li><a href="DeleteDeptUser.php">DeleteDeptUser</a> (부서사용자 등록정보 삭제)</li>
+        </ul>
+    </fieldset>
+    <br/>
+    <fieldset class="fieldset1">
+        <legend>포인트 관리 / 정액제 신청</legend>
+        <ul>
+            <li><a href="GetBalance.php">GetBalance</a> (연동회원 잔여포인트 확인)</li>
+            <li><a href="GetChargeURL.php">GetChargeURL</a> (연동회원 포인트충전 URL)</li>
+            <li><a href="GetPartnerBalance.php">GetPartnerBalance</a> (파트너 잔여포인트 확인)</li>
+            <li><a href="GetPartnerURL.php">GetPartnerURL</a> (파트너 포인트충전 URL)</li>
+            <li><a href="GetChargeInfo.php">GetChargeInfo</a> (과금정보 확인)</li>
+            <li><a href="GetFlatRatePopUpURL.php">GetFlatRatePopUpURL</a> (정액제 서비스 신청 URL)</li>
+            <li><a href="GetFlatRateState.php">GetFlatRateState</a> (정액제 서비스 상태 확인)</li>
+        </ul>
+    </fieldset>
+    <br/>
+    <fieldset class="fieldset1">
+        <legend>회원정보</legend>
+        <ul>
+            <li><a href="CheckIsMember.php">CheckIsMember</a> (연동회원 가입여부 확인)</li>
+            <li><a href="CheckID.php">CheckID</a> (아이디 중복 확인)</li>
+            <li><a href="JoinMember.php">JoinMember</a> (연동회원 신규가입)</li>
+            <li><a href="GetAccessURL.php">GetAccessURL</a> (팝빌 로그인 URL)</li>
+            <li><a href="GetCorpInfo.php">GetCorpInfo</a> (회사정보 확인)</li>
+            <li><a href="UpdateCorpInfo.php">UpdateCorpInfo</a> (회사정보 수정)</li>
+            <li><a href="RegistContact.php">RegistContact</a> (담당자 등록)</li>
+            <li><a href="ListContact.php">ListContact</a> (담당자 목록 확인)</li>
+            <li><a href="UpdateContact.php">UpdateContact</a> (담당자 정보 수정)</li>
+        </ul>
+    </fieldset>
 </body>
 </html>
