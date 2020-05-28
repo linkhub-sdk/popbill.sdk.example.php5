d270024e1f194db00d14c7be6568e74fed45db23
JohnJeong123 <frenchofkiss@gmail.com>
2020-05-28 09:40:33 +0900

    fixed updated date

--- EasyFinBankExample/common.php
+++ EasyFinBankExample/common.php
@@ -3,7 +3,7 @@
   * 팝빌 계좌조회 API PHP SDK Example
   *
   * PHP SDK 연동환경 설정방법 안내 : https://docs.popbill.com/easyfinbank/tutorial/php
-  * 업데이트 일자 : 2020-05-18
+  * 업데이트 일자 : 2020-05-28
   * 연동기술지원 연락처 : 1600-9854 / 070-4304-2991
   * 연동기술지원 이메일 : code@linkhub.co.kr
   */
