e03d95db2ba38b72e3d24976a58687d6e9356ddb
JohnJeong123 <frenchofkiss@gmail.com>
2020-06-25 20:26:01 +0900

    added CashbillService AssignMgtKey API

--- CashbillExample/index.php
+++ CashbillExample/index.php
@@ -61,6 +61,7 @@
             <li><a href="SendFAX.php">SendFAX</a> (팩스 전송)</li>
             <li><a href="ListEmailConfig.php">ListEmailConfig</a> (현금영수증 알림메일 전송목록 조회)</li>
             <li><a href="UpdateEmailConfig.php">UpdateEmailConfig</a> (현금영수증 알림메일 전송설정 수정)</li>
+            <li><a href="AssignMgtKey.php">AssignMgtKey</a> (문서번호 할당)</li>
         </ul>
     </fieldset>
     <fieldset class="fieldset1">
