783fc3cfbd4fc617c32cb462bb6f0fd69c30aa1f
ProWON <pro_won45@naver.com>
2021-08-04 14:45:16 +0900

    added Resopnse Cashbill Issue confirmNum, tradeDate

--- CashbillExample/Issue.php
+++ CashbillExample/Issue.php
@@ -31,6 +31,8 @@
         $result = $CashbillService->Issue($testCorpNum, $mgtKey, $memo, $testUserID, $emailSubject);
         $code = $result->code;
         $message = $result->message;
+        $confirmNum = $result->confirmNum;
+        $tradeDate = $result->tradeDate;
     }
     catch(PopbillException $pe) {
         $code = $pe->getCode();
@@ -46,6 +48,14 @@
                 <ul>
                     <li>Response.code : <?php echo $code ?></li>
                     <li>Response.message : <?php echo $message ?></li>
+                    <?php
+                      if ( isset($confirmNum) ) {
+                    ?>
+                      <li>Response.confirmNum : <?php echo $confirmNum ?></li>
+                      <li>Response.tradeDate : <?php echo $tradeDate ?></li>
+                    <?php
+                      }
+                    ?>
                 </ul>
             </fieldset>
          </div>
