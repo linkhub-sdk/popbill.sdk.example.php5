d872f0e362d9ea2f9f193b957e496c1bfbf7df33
ProWON <pro_won45@naver.com>
2021-12-23 16:18:56 +0900

    fixed email address, removec tel

--- EasyFinBankExample/common.php
+++ EasyFinBankExample/common.php
@@ -4,8 +4,8 @@
   *
   * PHP SDK 연동환경 설정방법 안내 : https://docs.popbill.com/easyfinbank/tutorial/php
   * 업데이트 일자 : 2021-12-23
-  * 연동기술지원 연락처 : 1600-9854 / 070-4304-2991
-  * 연동기술지원 이메일 : code@linkhub.co.kr
+  * 연동기술지원 연락처 : 1600-9854
+  * 연동기술지원 이메일 : code@linkhubcorp.com
   */
 
   require_once '../Popbill/PopbillEasyFinBank.php';
--- HTCashbillExample/common.php
+++ HTCashbillExample/common.php
@@ -4,8 +4,8 @@
   *
   * PHP SDK 연동환경 설정방법 안내 : https://docs.popbill.com/htcashbill/tutorial/php
   * 업데이트 일자 : 2021-07-19
-  * 연동기술지원 연락처 : 1600-9854 / 070-4304-2991
-  * 연동기술지원 이메일 : code@linkhub.co.kr
+  * 연동기술지원 연락처 : 1600-9854
+  * 연동기술지원 이메일 : code@linkhubcorp.com
   *
   * <테스트 연동개발 준비사항>
   * 1) 23, 26번 라인에 선언된 링크아이디(LinkID)와 비밀키(SecretKey)를
