e5d87030ef6bcd78ab48ce70e745ffbdf4731876
최상혁 <shchoi@linkhubcorp.com>
2022-10-04 15:08:26 +0900

    기업정보조회 추가

---
+++ BizInfoCheckExample/CheckBizInfo.php
@@ -0,0 +1,109 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="stylesheet" type="text/css" href="../Example.css" media="screen" />
+        <title>팝빌 SDK PHP 5.X Example.</title>
+    </head>
+<?php
+    /**
+     * 사업자번호 1건에 대한 기업정보를 확인합니다.
+     * - https://docs.popbill.com/bizinfocheck/php/api#CheckBizInfo
+     */
+
+    include 'common.php';
+
+    if ( isset($_GET['CorpNum']) && $_GET['CorpNum'] != '' ) {
+
+        // 팝빌회원 사업자번호
+        $MemberCorpNum = "1234567890";
+
+        // 조회할 사업자번호
+        $CheckCorpNum = $_GET['CorpNum'];
+
+        try {
+            $result = $BizInfoCheckService->checkBizInfo($MemberCorpNum, $CheckCorpNum);
+        }
+        catch (PopbillException $pe) {
+            $code = $pe->getCode();
+            $message = $pe->getMessage();
+        }
+    }
+?>
+    <body>
+        <div id="content">
+            <p class="heading1">Response</p>
+            <br/>
+            <fieldset class="fieldset1">
+                <legend>기업정보조회 - 단건</legend>
+                    <div class ="fieldset4">
+                    <form method= "GET" id="corpnum_form" action="CheckBizInfo.php">
+                        <input class= "txtCorpNum left" type="text" placeholder="사업자번호 기재" id="CorpNum" name="CorpNum" value ='<?php echo (isset($result->corpNum) ? $result->corpNum : "") ?>' tabindex=1/>
+                        <p class="find_btn find_btn01 hand" onclick="search()" tabindex=2>조회</p>
+                    </form>
+                    </div>
+            </fieldset>
+            <?php
+                if(isset($result)) {
+            ?>
+                <fieldset class="fieldset2">
+                    <legend>기업정보조회 - 단건</legend>
+                    <ul>
+                        <li>사업자번호 (corpNum) : <?php echo $result->corpNum?></li>
+                        <li>법인번호 (companyRegNum) : <?php echo $result->companyRegNum?></li>
+                        <li>확인일시 (checkDT) : <?php echo $result->checkDT?></li>
+                        <li>상호 (corpName) : <?php echo $result->corpName?></li>
+                        <li>기업형태코드 (corpCode) : <?php echo $result->corpCode?></li>
+                        <li>기업규모코드 (corpScaleCode) : <?php echo $result->corpScaleCode?></li>
+                        <li>개인법인코드 (personCorpCode) : <?php echo $result->personCorpCode?></li>
+                        <li>본점지점코드 (headOfficeCode) : <?php echo $result->headOfficeCode?></li>
+                        <li>산업코드 (industryCode) : <?php echo $result->industryCode?></li>
+                        <li>설립구분코드 (establishCode) : <?php echo $result->establishCode?></li>
+                        <li>설립일자 (establishDate) : <?php echo $result->establishDate?></li>
+                        <li>대표자명 (CEOName) : <?php echo $result->ceoname?></li>
+                        <li>사업장구분코드 (workPlaceCode) : <?php echo $result->workPlaceCode?></li>
+                        <li>주소구분코드 (addrCode) : <?php echo $result->addrCode?></li>
+                        <li>우편번호 (zipCode) : <?php echo $result->zipCode?></li>
+                        <li>주소 (addr) : <?php echo $result->addr?></li>
+                        <li>상세주소 (addrDetail) : <?php echo $result->addrDetail?></li>
+                        <li>영문주소 (enAddr) : <?php echo $result->enAddr?></li>
+                        <li>업종 (bizClass) : <?php echo $result->bizClass?></li>
+                        <li>업태 (bizType) : <?php echo $result->bizType?></li>
+                        <li>결과코드 (result) : <?php echo $result->result?></li>
+                        <li>결과메시지 (resultMessage) : <?php echo $result->resultMessage?></li>
+                        <li>사업자과세유형 (closeDownTaxType) : <?php echo $result->closeDownTaxType?></li>
+                        <li>과세유형전환일자 (closeDownTaxTypeDate) : <?php echo $result->closeDownTaxTypeDate?></li>
+                        <li>휴폐업상태 (closeDownState) : <?php echo $result->closeDownState?></li>
+                        <li>휴폐업일자 (closeDownStateDate) : <?php echo $result->closeDownStateDate?></li>
+                    
+                    </ul>
+                </fieldset>
+
+            <?php
+                } if(isset($code)) {
+            ?>
+
+                <fieldset class="fieldset2">
+                    <legend>기업정보조회 - 단건</legend>
+                    <ul>
+                        <li>Response.code : <?php echo $code ?> </li>
+                        <li>Response.message : <?php echo $message ?></li>
+                    </ul>
+                </fieldset>
+            <?php
+                }
+            ?>
+
+         </div>
+
+          <script type ="text/javascript">
+         window.onload=function(){
+             document.getElementById('CorpNum').focus();
+         }
+
+         function search(){
+            document.getElementById('corpnum_form').submit();
+         }
+
+         </script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -11,19 +11,20 @@
     <fieldset class="fieldset1">
         <legend>팝빌 API 목록</legend>
         <ul>
-            <li><a href="TaxinvoiceExample">TaxinvoiceService</a> - 전자세금계산서 API</li>
-            <li><a href="StatementExample">StatementService</a> - 전자명세서 API</li>
-            <li><a href="CashbillExample">CashbillService</a> - 현금영수증 API</li>
+            <li><a href="TaxinvoiceExample">TaxinvoiceExample</a> - 전자세금계산서 API</li>
+            <li><a href="StatementExample">StatementExample</a> - 전자명세서 API</li>
+            <li><a href="CashbillExample">CashbillExample</a> - 현금영수증 API</li>
 
             <li><a href="HTTaxinvoiceExample">HTTaxinvoiceExample</a> - 홈택스연동 (전자세금계산서) API</li>
             <li><a href="HTCashbillExample">HTCashbillExample</a> - 홈택스연동 (현금영수증) API</li>
-            <li><a href="ClosedownExample">ClosedownService</a> - 휴폐업조회 API</li>
-            <li><a href="EasyFinBankExample">EasyFinBankService</a> - 계좌조회 API</li>
-            <li><a href="AccountCheckExample">AccountCheckService</a> - 예금주조회 API</li>
+            <li><a href="ClosedownExample">ClosedownExample</a> - 휴폐업조회 API</li>
+            <li><a href="BizInfoCheckExample">BizInfoCheckExample</a> - 기업정보조회 API</li>
+            <li><a href="EasyFinBankExample">EasyFinBankExample</a> - 계좌조회 API</li>
+            <li><a href="AccountCheckExample">AccountCheckExample</a> - 예금주조회 API</li>
 
-            <li><a href="MessageExample">MessageService</a> - 문자메시지 API</li>
-            <li><a href="FaxExample">FaxService</a> - 팩스 API</li>
-            <li><a href="KakaoExample">KakaoService</a> - 카카오톡 API</li>
+            <li><a href="MessageExample">MessageExample</a> - 문자메시지 API</li>
+            <li><a href="FaxExample">FaxExample</a> - 팩스 API</li>
+            <li><a href="KakaoExample">KakaoExample</a> - 카카오톡 API</li>
         </ul>
     </fieldset>
 </div>
