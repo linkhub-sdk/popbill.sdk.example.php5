42a418f5fcbe3703795c8e2a63fe73d77d3ff7cf
AlBaneo93 <albaneo0724@gmail.com>
2023-04-19 13:35:21 +0900

    현금영수증 - 환불 가능 포인트 조회 view 추가

---
+++ CashbillExample/GetRefundablePoint.php
@@ -0,0 +1,45 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="../Example.css" media="screen" />
+    <title>팝빌 SDK PHP 5.X Example.</title>
+</head>
+<?php
+/**
+ * 팝빌 사이트에 로그인 상태로 접근할 수 있는 페이지의 팝업 URL을 반환합니다.
+ * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
+ * - https://developers.popbill.com/reference/bizinfocheck/php/api/member#GetAccessURL
+ */
+
+include 'common.php';
+
+// 팝빌 회원 사업자 번호, "-"제외 10자리
+$testCorpNum = '1234567890';
+
+// 팝빌 회원 아이디
+$testUserID = 'testkorea';
+
+try {
+
+    $result = $CashbillService->GetRefundablePoint($testCorpNum, $testUserID);
+} catch (PopbillException $pe) {
+    $code = $pe->getCode();
+    $message = $pe->getMessage();
+}
+?>
+
+<body>
+    <div id="content">
+        <p class="heading1">Response</p>
+        <br />
+        <fieldset class="fieldset1">
+            <legend>팝빌 로그인 URL</legend>
+            <ul>
+                <li>refundableBalance (환불 가능 포인트) : <?php $result->refundableBalance ?></li>
+            </ul>
+        </fieldset>
+    </div>
+</body>
+
+</html>
