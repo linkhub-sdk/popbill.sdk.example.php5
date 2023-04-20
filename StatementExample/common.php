fee2b4fc5ce1123861a54a4e4decd4ac00209c56
AlBan93 <albaneo0724@gmail.com>
2023-04-20 09:34:33 +0900

    업데이트 일자정보 수정

--- StatementExample/common.php
+++ StatementExample/common.php
@@ -3,7 +3,7 @@
   * 팝빌 전자명세서 API PHP SDK Example
   *
   * PHP SDK 연동환경 설정방법 안내 : https://developers.popbill.com/guide/statement/php/getting-started/tutorial
-  * 업데이트 일자 : 2023-02-17
+  * 업데이트 일자 : 2023-04-20
   * 연동기술지원 연락처 : 1600-9854
   * 연동기술지원 이메일 : code@linkhubcorp.com
   *
